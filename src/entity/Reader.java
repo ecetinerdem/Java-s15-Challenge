3b42de9c0fc685f605e13f5a794000d35f0d25c3
Cetin Erdem <cetoerdem1@gmail.com>
2025-01-21 00:28:10 +0400

    Update Reader.java

--- src/entity/Reader.java
+++ src/entity/Reader.java
@@ -21,7 +21,10 @@ public class Reader extends Person{
         return "Reader " + name;
     }
 
-    public boolean canBorrowMore() {
-        return  borrowedBooks.size() < bookBorrowLimit;
+    public String canBorrowMore() {
+        if (borrowedBooks.size() >= bookBorrowLimit) {
+            return "Sorry you cannot borrow anymore books until you return previously borrowed books";
+        }
+        return "Book successfully borrowed";
     }
 }
