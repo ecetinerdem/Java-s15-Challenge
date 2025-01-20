6b6d1272664e5512ad664090532c651278b064e4
Cetin Erdem <cetoerdem1@gmail.com>
2025-01-20 23:49:57 +0400

    Update Person.java

--- src/entity/Person.java
+++ src/entity/Person.java
@@ -13,5 +13,27 @@ public abstract class Person {
 
     public abstract String whoYouAre();
 
-    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
