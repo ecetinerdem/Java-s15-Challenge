0ebcb2c60228551bdd07f5e35372cc98cd366303
Cetin Erdem <cetoerdem1@gmail.com>
2025-01-21 00:03:31 +0400

    Update Book.java

--- src/entity/Book.java
+++ src/entity/Book.java
@@ -1,9 +1,26 @@
 package entity;
 
+import java.time.LocalDate;
+
 public class Book {
     private Long id;
     private String name;
     private Author author;
     private Double price;
     private Status status;
+    private LocalDate purchaseDate;
+    private LocalDate returnDate;
+    private Category category;
+
+
+    public Book(Long id, String name, Author author, Double price, Status status, LocalDate purchaseDate, LocalDate returnDate, Category category) {
+        this.id = id;
+        this.name = name;
+        this.author = author;
+        this.price = price;
+        this.status = status;
+        this.purchaseDate = purchaseDate;
+        this.returnDate = returnDate;
+        this.category = category;
+    }
 }
