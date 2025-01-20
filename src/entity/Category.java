fdd7a7151b9d4e6502dcdaf6f5e9105835b7a49c
Cetin Erdem <cetoerdem1@gmail.com>
2025-01-21 00:03:34 +0400

    Create Category.java

---
+++ src/entity/Category.java
@@ -0,0 +1,15 @@
+package entity;
+
+public enum Category {
+    HORROR,
+    SCIFI,
+    FANTASY,
+    HISTORY,
+    LITERATURE,
+    ACTION,
+    NOVEL,
+    COMICS,
+    SCIENCE,
+    HOBBY,
+    LEARNING
+}
