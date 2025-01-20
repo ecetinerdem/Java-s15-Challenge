8350d6b0d0a76423b00ce490847ef9970695dd02
Cetin Erdem <cetoerdem1@gmail.com>
2025-01-20 23:46:01 +0400

    Update Person.java

--- src/entity/Person.java
+++ src/entity/Person.java
@@ -1,5 +1,17 @@
 package entity;
 
 public abstract class Person {
+    private Long id;
+    private String name;
+    private String email;
+
+    public Person(Long id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public abstract String whoYouAre();
+
     
 }
