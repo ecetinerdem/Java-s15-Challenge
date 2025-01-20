1dc6cb774f50257c198908eeb23e581345d22be0
Cetin Erdem <cetoerdem1@gmail.com>
2025-01-20 23:51:19 +0400

    Update Author.java

--- src/entity/Author.java
+++ src/entity/Author.java
@@ -1,11 +1,16 @@
 package entity;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Author extends Person{
 
     private Set<Book> books;
 
     public Author (Long id, String name, String email) {
-        super()
+        super(id, name, email);
+        this.books = new HashSet<>();
+
     }
 
 
